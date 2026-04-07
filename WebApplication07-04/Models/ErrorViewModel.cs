c661fbf222a3b86b6ebbeb1e8b3a7e5f49eff704
kashafraza <mdkashafraza@gmail.com>
2026-04-07 09:59:45 +0530

    Add project files.

---
+++ WebApplication07-04/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication07_04.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
